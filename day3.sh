9ec7c14d8377afb73966effc64a29e7bae07cb09
Ahmed Elessawy <blueribbon2008@gmail.com>
2021-01-30 20:45:37 +0200

    edit 3rd script

--- day3.sh
+++ day3.sh
@@ -5,3 +5,11 @@ WORD="script"
 echo "${WORD}ing is fun!"
 NUM="----------"
 echo "${NUM} ${NUM} "
+
+REPLACE="read"
+
+echo "you should be able to ${REPLACE} basic scripts"
+
+REPLACE="master"
+
+echo "we can re-assign any variable in the script as this example to ${REPLACE} scripting"
