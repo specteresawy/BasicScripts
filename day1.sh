4e5d2ec68c8fa7d2d2b92ea912c5f233b70a9905
Ahmed Elessawy <blueribbon2008@gmail.com>
2021-01-30 20:48:04 +0200

    update some files

--- day1.sh
+++ day1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Script used for variable practice
 echo "I don't have to be great to start, but I have to start to be great!"
 echo "__________"
 
--- day2.sh
+++ day2.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+#variable assignment 
+
+
 SKILL="shell scripting"
+# echo the following sentence 
+
 echo "I want to be good at ${SKILL}, thats why I practice ${SKILL}"
+# more variable assignments
+
 target="Cybersecurity"
 learning="computer science"
+
 echo "I am learning ${learning} to become professional at ${target}"
+
 #this is a comment
 # upper case is just a convention for variables in your scripts
 
