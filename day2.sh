6615fcc4bca24e32f08b89ad5d4ca1e2eb9293c7
Ahmed Elessawy <blueribbon2008@gmail.com>
2021-01-30 20:33:10 +0200

    adding comments

--- day2.sh
+++ day2.sh
@@ -4,3 +4,6 @@ echo "I want to be good at ${SKILL}, thats why I practice ${SKILL}"
 target="Cybersecurity"
 learning="computer science"
 echo "I am learning ${learning} to become professional at ${target}"
+#this is a comment
+# upper case is just a convention for variables in your scripts
+
