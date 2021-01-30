0e9935d02dde2dea011ec6f743389de14b0b02a5
Ahmed Elessawy <blueribbon2008@gmail.com>
2021-01-30 19:14:50 +0200

    update second script

--- day2.sh
+++ day2.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 SKILL="shell scripting"
 echo "I want to be good at ${SKILL}, thats why I practice ${SKILL}"
+target="Cybersecurity"
+learning="computer science"
+echo "I am learning ${learning} to become professional at ${target}"
